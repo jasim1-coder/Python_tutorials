a4656c48e747e8a5c6a753c3253b33cf096b853f
jasim <tjasim786@.com>
2024-08-16 20:40:32 +0530

    second commit

--- helloapp/views.py
+++ helloapp/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render
 # Create your views here.
 def print_hello(request):
-    return HttpResponse("hello")
+    movies_data ={
+        'movies': [
+        {
+        'title': 'godfather',
+        'year': '1997',
+        'summary': 'story of a under world king',
+        'success': False,
+        },
+        {
+        'title': 'Batman',
+        'year': '2000',
+        'summary': 'story of a unknownvhero',
+        'success': True,
+        },
+        {
+        'title': 'spiderman',
+        'year': '2005',
+        'summary': 'rise of a new star',
+        'success': True,
+        },
+        {
+        'title': 'redemption',
+        'year': '1995',
+        'summary': 'story of a prisnor',
+        'success': False,
+        },
+        {
+        'title': 'king',
+        'year': '1997',
+        'summary': '',
+        'success': False,
+        },
+
+    ]}
+    return render(request, 'hello.html', movies_data)
